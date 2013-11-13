ceb1fdf957e42e057e222a7a3ef5a7ac055a9966
sds-logigear <nguyen.le@logigear.com>
2013-11-13 16:49:22 +0700

    Changed source code follow comments. So I have to delete some test case and change some configure on layout.

--- SimpleCalc/src/com/example/simplecalc/MainActivity.java
+++ SimpleCalc/src/com/example/simplecalc/MainActivity.java
@@ -4,45 +4,75 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private static String LOG_TAG = "MainScreen";
+    private EditText value1;
+    private EditText value2;
+    private TextView result;
+
+    public static enum Operation {
+        ADD, MULTIPLY
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        final String LOG_TAG = "MainScreen";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText value1 = (EditText) findViewById(R.id.value1);
-        final EditText value2 = (EditText) findViewById(R.id.value2);
-        final TextView result = (TextView) findViewById(R.id.result);
+        value1 = (EditText) findViewById(R.id.value1);
+        value2 = (EditText) findViewById(R.id.value2);
+        result = (TextView) findViewById(R.id.result);
         Button addButton = (Button) findViewById(R.id.addValues);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    double val1 = Double.parseDouble(value1.getText().toString());
-                    double val2 = Double.parseDouble(value2.getText().toString());
-                    Double answer = val1 + val2;
-                    result.setText(answer.toString());
-                } catch (NumberFormatException e) {
-                    Log.e(LOG_TAG, "Failed to add numbers ", e);
-                }
-            }
-        });
+        addButton.setOnClickListener(buttonAddClick);
         Button multiplyButton = (Button) findViewById(R.id.multiplyValues);
-        multiplyButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                try {
-                    double val1 = Double.parseDouble(value1.getText().toString());
-                    double val2 = Double.parseDouble(value2.getText().toString());
-                    Double answer = val1 * val2;
-                    result.setText(answer.toString());
-                } catch (Exception e) {
-                    Log.e(LOG_TAG, "Failed to multiply numbers ", e);
-                }
-            }
-        });
+        multiplyButton.setOnClickListener(buttonMultiplyClick);
     }
+
+    private OnClickListener buttonAddClick = new OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+                Integer answer = getResultOperation(Operation.ADD);
+                result.setText(answer.toString());
+        }
+    };
+
+    private OnClickListener buttonMultiplyClick = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+                Integer answer = getResultOperation(Operation.MULTIPLY);
+                result.setText(answer.toString());
+        }
+    };
+
+    /**
+     * Gets the result operation.
+     * 
+     * @param operation the operation such as: add, multiply
+     * @return the result operation as {@link Integer}
+     */
+    private Integer getResultOperation(Operation operation) {
+        Integer answer = 0;
+        try {
+            int val1 = Integer.parseInt(value1.getText().toString());
+            int val2 = Integer.parseInt(value2.getText().toString());
+            switch (operation) {
+            case ADD:
+                answer = val1 + val2;
+                break;
+            case MULTIPLY:
+                answer = val1 * val2;
+                break;
+            default:
+                break;
+            }
+        } catch (NumberFormatException e) {
+            Log.e(LOG_TAG, "Failed to add numbers ", e);
+        }
+        return answer;
+    }
+
 }
--- SimpleCalcTest/src/com/example/simplecalc/test/MathTest.java
+++ SimpleCalcTest/src/com/example/simplecalc/test/MathTest.java
@@ -22,17 +22,14 @@ public class MathTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private static final String NUMBER_24 = "2 4 ENTER ";
     private static final String NUMBER_74 = "7 4 ENTER ";
-    private static final String NUMBER_5_DOT_5 = "5 PERIOD 8 ENTER ";
     private static final String NUMBER_NEG_22 = "MINUS 2 2 ENTER ";
     private static final String NUMBER_0 = "0 ENTER ";
 
-    private static final String ADD_RESULT = "98.0";
-    private static final String ADD_NEGATIVE_RESULT = "52.0";
-    private static final String ADD_DECIMAL_RESULT = "79.8";
-    private static final String MULTIPLY_RESULT = "1776.0";
-    private static final String MULTIPLY_DECIMAL_RESULT = "429.2";
-    private static final String MULTIPLY_NEGATIVE_RESULT = "-1628.0";
-    private static final String MULTIPLY_ZERO_RESULT = "0.0";
+    private static final String ADD_RESULT = "98";
+    private static final String ADD_NEGATIVE_RESULT = "52";
+    private static final String MULTIPLY_RESULT = "1776";
+    private static final String MULTIPLY_NEGATIVE_RESULT = "-1628";
+    private static final String MULTIPLY_ZERO_RESULT = "0";
 
     public void testAddValues() {
         // we use sendKeys instead of setText so it goes through entry
@@ -44,46 +41,34 @@ public class MathTest extends ActivityInstrumentationTestCase2<MainActivity> {
         sendKeys("ENTER");
         // get result
         String mathResult = result.getText().toString();
-        compareValue(ADD_RESULT, mathResult);
-    }
-
-    public void testAddDecimalValues() {
-        sendKeys(NUMBER_5_DOT_5 + NUMBER_74 + "ENTER");
-        String mathResult = result.getText().toString();
-        compareValue(ADD_DECIMAL_RESULT, mathResult);
+        assertValue(ADD_RESULT, mathResult);
     }
 
     public void testAddSubtractValues() {
         sendKeys(NUMBER_NEG_22 + NUMBER_74 + "ENTER");
         String mathResult = result.getText().toString();
-        compareValue(ADD_NEGATIVE_RESULT, mathResult);
+        assertValue(ADD_NEGATIVE_RESULT, mathResult);
     }
 
     public void testMultiplyValues() {
         sendKeys(NUMBER_24 + NUMBER_74 + " DPAD_RIGHT ENTER");
         String mathResult = result.getText().toString();
-        compareValue(MULTIPLY_RESULT, mathResult);
-    }
-
-    public void testMultiplyDecimalValues() {
-        sendKeys(NUMBER_5_DOT_5 + NUMBER_74 + " DPAD_RIGHT ENTER");
-        String mathResult = result.getText().toString();
-        compareValue(MULTIPLY_DECIMAL_RESULT, mathResult);
+        assertValue(MULTIPLY_RESULT, mathResult);
     }
 
     public void testMultiplySubtractValues() {
         sendKeys(NUMBER_NEG_22 + NUMBER_74 + " DPAD_RIGHT ENTER");
         String mathResult = result.getText().toString();
-        compareValue(MULTIPLY_NEGATIVE_RESULT, mathResult);
+        assertValue(MULTIPLY_NEGATIVE_RESULT, mathResult);
     }
 
-    public void testMultiptyZeroValues() {
+    public void testMultiplyZeroValues() {
         sendKeys(NUMBER_0 + NUMBER_74 + " DPAD_RIGHT ENTER");
         String mathResult = result.getText().toString();
-        compareValue(MULTIPLY_ZERO_RESULT, mathResult);
+        assertValue(MULTIPLY_ZERO_RESULT, mathResult);
     }
 
-    private void compareValue(String expectedValue, String actualValue) {
+    private void assertValue(String expectedValue, String actualValue) {
         assertTrue("Add result should be " + expectedValue + " but was " + actualValue,
                 actualValue.equals(expectedValue));
     }
